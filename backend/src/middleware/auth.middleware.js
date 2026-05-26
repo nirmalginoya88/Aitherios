8aeaba1a2ef4ae5a7dbc9a54001a22c6c61b5479
nirmalginoya88 <nirmalginoya88@gmail.com>
2026-05-26 11:07:39 +0530

    Fixed Middlerware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -28,8 +28,6 @@ const protect = async (req, res, next) => {
 };
 
 //Rate Limitting Middleware 
-const rateLimit = require('express-rate-limit');
-
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per windowMs
