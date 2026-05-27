682b5b03f70029a866c58394fd112f2450fe37a7
nirmalginoya88 <nirmalginoya88@gmail.com>
2026-05-27 19:05:13 +0530

    Allow guest users to browse products without authentication

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -4,8 +4,8 @@ const { createProduct, getProducts, getProduct, updateProduct, deleteProduct } =
 const { upload } = require('../middleware/multer.middleware');
 const { protect, adminOnly } = require('../middleware/auth.middleware');
 
-router.get('/', protect, getProducts);
-router.get('/:name', protect, getProduct);
+router.get('/', getProducts);
+router.get('/:name', getProduct);
 router.post('/', protect, adminOnly, upload.array('images', 10), createProduct);
 router.put('/:id', protect, adminOnly, upload.array('images', 10), updateProduct);
 router.delete('/:id', protect, adminOnly, deleteProduct);
